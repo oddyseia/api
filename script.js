4f3443b1b0e4db24e404fe51d53adc436c3e2fd1
Murat Altay <murat.altay@farmasi.com.tr>
2024-02-29 20:37:21 +0300

    ✨  revize [ADDED] modal

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 // API'den veri çekme
-fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-  .then(response => response.json()) // verilerin json formata dönüşmesi
-  .then(data => {
+function openModal(pokemonIndex) {
+  const pokemonInfoUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`;
+  fetch(pokemonInfoUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("modal").style.display = "flex";
+
+      const modalContent = document.getElementById("modalContent");
+      const kartResim = document.createElement("img");
+      kartResim.className = "modal-image";
+      kartResim.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
+      kartResim.alt = `Pokemon ${pokemonIndex}`;
+      modalContent.appendChild(kartResim);
+
+      const kartBaslik = document.createElement("div");
+      kartBaslik.className = "modal-text bold";
+      kartBaslik.textContent = data.name;
+      modalContent.appendChild(kartBaslik);
+    });
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+  const modalContent = document.getElementById("modalContent");
+  modalContent.innerHTML = "";
+}
+
+fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+  .then((response) => response.json()) // verilerin json formata dönüşmesi
+  .then((data) => {
     // pokemon isimlerini çekme
     const pokemons = data.results;
 
@@ -10,36 +37,23 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
       const pokemonIndex = index + 1;
       const pokemonInfoUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`;
 
+      const kart = document.createElement("div");
+      kart.className = "kart";
+      kart.onclick = () => openModal(pokemonIndex);
+
+      const kartResim = document.createElement("img");
+      kartResim.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
+      kartResim.alt = `Pokemon ${pokemonIndex}`;
+      kart.appendChild(kartResim);
+
+      const kartIcerik = document.createElement("div");
+      kartIcerik.className = "kart-icerik";
+
+      const kartBaslik = document.createElement("h2");
+      kartBaslik.textContent = data.name;
+      kartIcerik.appendChild(kartBaslik);
+      kart.appendChild(kartIcerik);
+      document.getElementById("pokemon-kartları").appendChild(kart);
       // her pokemon için ayrı ayrı API'den veriyi çekme
-      fetch(pokemonInfoUrl)
-        .then(response => response.json())
-        .then(data => {
-          // kart içeriğini oluşturma
-          const kart = document.createElement('div');
-          kart.className = 'kart';
-
-          const kartResim = document.createElement('img');
-          kartResim.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
-          kartResim.alt = `Pokemon ${pokemonIndex}`;
-          kart.appendChild(kartResim);
-
-          const kartIcerik = document.createElement('div');
-          kartIcerik.className = 'kart-icerik';
-
-          const kartBaslik = document.createElement('h2');
-          kartBaslik.textContent = data.name;
-          kartIcerik.appendChild(kartBaslik);
-
-          const kartParagraf = document.createElement('p');
-          kartParagraf.textContent = `ID: ${data.id}`;
-          kartIcerik.appendChild(kartParagraf);
-
-          const kartParagraf2 = document.createElement('p');
-          kartParagraf2.textContent = `Türler: ${data.types.map(type => type.type.name).join(', ')}`;
-          kartIcerik.appendChild(kartParagraf2);
-
-          kart.appendChild(kartIcerik);
-          document.getElementById('pokemon-kartları').appendChild(kart);
-        })
     });
-  })
+  });
